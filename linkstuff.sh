9708e6730f43962614c3903d8f3e4016217af611
Emre Guneyler <eguneys@gmail.com>
2023-01-15 23:16:44 +0300

    github

---
+++ linkstuff.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/eguneys/lchessanalysis
+cd lchessanalysis
+pnpm install
+pnpm validate
+cd ..
+git clone https://github.com/eguneys/solid-play
+cd solid-play
+pnpm install
+pnpm build
+cd ..
+git clone https://github.com/eguneys/chessboard23
+cd chessboard23
+pnpm install
+pnpm build
