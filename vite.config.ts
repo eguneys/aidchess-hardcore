3cbf5ddc9b7eb21bcb429c614a8f5ee8dbb1d897
Emre Guneyler <eguneys@gmail.com>
2023-01-16 08:10:33 +0300

    pwa"

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,26 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   base: './',
   plugins: [
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({ 
+      devOptions: {
+        enabled: false
+      },
+      registerType: 'autoUpdate', 
+      manifest: {
+      name: 'Aidchess',
+      short_name: 'Aidchess',
+      description: 'Aidchess Hardcore Chess',
+      theme_color: '#ffffff',
+      icons: [{
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      }, {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }]
+    } }),
     solidPlugin({ hot : false }) 
   ]
 })
