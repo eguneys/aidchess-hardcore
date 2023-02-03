d611165932a521229c5edfe8ecc855957e9a4c70
Emre Guneyler <eguneys@gmail.com>
2023-02-03 15:16:30 +0300

    lazy

--- src/myapp.tsx
+++ src/myapp.tsx
@@ -1,21 +1,14 @@
 import './base.scss'
 import { createSignal, createEffect, on } from 'solid-js'
 import { hashIntegration, useNavigate, Router, Routes, Route, A} from '@solidjs/router'
-//import { lazy } from 'solid-js'
+import { lazy } from 'solid-js'
 import { Socket, generic_handlers } from './socket'
 import { MetaProvider } from '@solidjs/meta'
 
-/*
 const About = lazy(() => import('./about'))
 const MainPage = lazy(() => import('./home'))
 const Hardcore = lazy(() => import('./hardcore'))
 const Predict = lazy(() => import('./predict'))
-*/
-
-import About from './about'
-import MainPage from './home'
-import Hardcore from './hardcore'
-import Predict from './predict'
 
 type Redirect = { redirect: string }
 
