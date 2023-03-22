a02729e79a991ac530f6e4f1b3e0265dfbaca869
eguneys <eguneys@gmail.com>
2023-03-22 13:37:16 +0000

    Deploying to gh-pages from @ eguneys/aidchess-hardcore@57690a34d7e665d4cc11444c93b6f6343c6426a0 🚀

--- assets/opening-622192b2.js
+++
@@ -1,1 +0,0 @@
-import{c as e,T as t,f as n}from"./index-97627c47.js";import"./solid-play-6c62466f.js";import"./util-2d6699b1.js";import"./chess-ece2668e.js";const c=()=>e(t,{children:" aidchess.com - Openings Exercise "});n(["click"]);export{c as default};
